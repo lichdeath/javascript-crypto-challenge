e83e61c05cc66343b4897faea4df0ada63749a9f
mao uyen tram <maoketram@hotmail.com>
2019-10-15 19:40:50 +0200

    done

--- src/SecureSession.js
+++ src/SecureSession.js
@@ -8,10 +8,39 @@ let keypair = null;
 
 })();
 
-module.exports.setClientPublicKey = async function SetClientPublicKey(ClientPublicKey){
-    ClientKey = ClientPublicKey;
+module.exports.setClientPublicKey = function SetClientPublicKey(ClientPublicKey){
+
+        if (ClientKey == null ||ClientKey == ClientPublicKey ) {
+            ClientKey = ClientPublicKey;
+        }
+        else {
+            throw 'client public key already set';
+        }
+
 };
 
-module.exports.ServerPublickKey = async ()=>{
+module.exports.serverPublicKey = async ()=>{
     return keypair.publicKey;
+};
+
+module.exports.decrypt = async (cyphertext, nonce)=>{
+    await nacl.ready;
+    let sharedkey = nacl.crypto_kx_server_session_keys(
+        keypair.publicKey,
+        keypair.privateKey,
+        ClientKey
+    );
+    return nacl.crypto_secretbox_open_easy(cyphertext,nonce,sharedkey.sharedRx);
+};
+
+module.exports.encrypt = async (msg)=>{
+    await nacl.ready;
+    let sharedkey = nacl.crypto_kx_server_session_keys(
+        keypair.publicKey,
+        keypair.privateKey,
+        ClientKey
+    );
+    let nonce = nacl.randombytes_buf(nacl.crypto_secretbox_NONCEBYTES)
+    let cypher = nacl.crypto_secretbox_easy(msg,nonce,sharedkey.sharedTx);
+    return {ciphertext: cypher,nonce: nonce};
 };
